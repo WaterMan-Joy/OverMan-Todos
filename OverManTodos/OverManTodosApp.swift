3d1a5267bc64abfb57b37b02c1d478aff91c11d5
WaterMan-Joy <nietzsche21st@gmail.com>
2023-07-06 12:28:23 +0900

    first commit

---
+++ OverManTodos/OverManTodosApp.swift
@@ -0,0 +1,20 @@
+//
+//  OverManTodosApp.swift
+//  OverManTodos
+//
+//  Created by 김종희 on 2023/07/06.
+//
+
+import SwiftUI
+
+@main
+struct OverManTodosApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
